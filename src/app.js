ec1006955f4c342eaa7a4fb80eec0041cbef26c5
Tomas Oyarzun <xilent312@gmail.com>
2022-09-26 13:54:16 -0300

    new branch

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const routes = require('./routes/index')
+const cors = require('cors')
 const app = express()
 
-
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(routes)
