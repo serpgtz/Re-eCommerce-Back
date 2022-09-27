f4d6a0282c3e1eb8892d82871b5fe47acc5f75bd
Chaos <antonioscutari2@hotmail.com>
2022-09-27 00:50:10 -0300

    name => username

--- src/models/User.js
+++ src/models/User.js
@@ -1,31 +1,29 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-    name : {
-        type : String,
-        required : true,
-    },
+  username: {
+    type: String,
+    required: true,
+  },
 
-    email : {
-        type : String,
-        Uniquie : true,
-        required : true,
-        
-    },
+  email: {
+    type: String,
+    Uniquie: true,
+    required: true,
+  },
 
-    password : {
-        type : String,
-        required : true,
-    },
+  password: {
+    type: String,
+    required: true,
+  },
 
-    /* 
+  /* 
      
     
     
     */
-})
+});
 
-const User = mongoose.model('User', UserSchema)
+const User = mongoose.model("User", UserSchema);
 
-module.exports = User
+module.exports = User;
