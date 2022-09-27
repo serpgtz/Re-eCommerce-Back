cf7266493d41dadfd978d7f780b7edca1e457f26
Chaos <antonioscutari2@hotmail.com>
2022-09-27 02:23:49 -0300

    added full CRUD routes

--- src/routes/userRoutes/user.routes.js
+++ src/routes/userRoutes/user.routes.js
@@ -1,7 +1,17 @@
 const { Router } = require("express");
-const createNewUser = require("../../controllers/user.controller");
+const {
+  getUser,
+  createNewUser,
+  getAllUsers,
+  modifyUser,
+  deleteUser,
+} = require("../../controllers/user.controller");
 const router = Router();
 
 router.post("/register", createNewUser);
+router.get("/users", getAllUsers);
+router.get("/users/:id", getUser);
+router.put("/users/:id", modifyUser);
+router.delete("/users/:id", deleteUser);
 
 module.exports = router;
