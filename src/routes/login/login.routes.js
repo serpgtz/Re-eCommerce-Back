64f3273e7cdc7584a53fa57f354751178ec270c4
Tomas Oyarzun <xilent312@gmail.com>
2022-09-27 20:58:12 -0300

    add routes login

--- src/routes/login/login.routes.js
+++ src/routes/login/login.routes.js
@@ -1,6 +1,12 @@
 const { Router } = require("express");
-const { confirmUser } = require("../../controllers/login.controller");
+const { registerPost ,authenticate  , confirmUser, changePassword , forgotPassword} = require("../../controllers/login.controller");
 
 const router = Router();
 
+router.post('/register', registerPost )
+router.post('/login', authenticate )
+router.get('/confirmar/:token', confirmUser)
+router.get('/olvide-password/:token', forgotPassword)
+router.post('/olvide-password/:token', changePassword)
+ // router.get('/perfil',checkAuth, perfil )
 module.exports = router;
