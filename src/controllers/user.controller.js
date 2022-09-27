d9f1440d53ca6e4f2160fadebbe3cfda7b3ad7cf
Chaos <antonioscutari2@hotmail.com>
2022-09-27 02:23:21 -0300

    added full CRUD controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -13,4 +13,58 @@ const createNewUser = async (req, res) => {
     res.status(500).json(error);
   }
 };
-module.exports = createNewUser;
+
+const modifyUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  // const query = req.query.new;
+  try {
+    //Sólo en caso de emergencia
+    //   const users = query
+    //     ? await User.find().sort({ _id: -1 }).limit(5)
+    //     : await User.find();
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).send("USER DELETED");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+module.exports = {
+  createNewUser,
+  modifyUser,
+  getUser,
+  getAllUsers,
+  deleteUser,
+};
