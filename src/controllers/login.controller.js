eae8716467b551b161b6fb41fdc36039b7be7674
Tomas Oyarzun <xilent312@gmail.com>
2022-09-28 00:49:51 -0300

    jsonwebtoken install

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -21,12 +21,31 @@ const registerPost = async (req, res) => {
     console.log(error);
   }
 };
-const confirmUser = () => {};
+const confirmUser = async (req, res) => {
+    const {token} = req.params
+  
+  try {
+    const confirmedUser = await User.findOne({token})
+    if(!confirmedUser){
+      
+        return res.status(400).json({msg: 'token no valido'})
+    }else {
+
+        confirmedUser.token = null
+        confirmedUser.confirmed = true
+        await confirmedUser.save()
+
+        res.json({msg: 'usuario registrado correctamente'})
+    }
+  } catch (error) {
+    console.log(error)
+  }
+};
 
 const authenticate = async (req, res) => {
   const user = await User.findOne({ username: req.body.username });
   !user && res.status(401).send("¡Usuario no existe!");
-  !user.confirmado && res.status(401).send("¡Usuario no confirmado!");
+  user.confirmed === false && res.status(401).send("¡Usuario no confirmado!");
   const hashPass = CryptoJS.AES.decrypt(
     user.password,
     process.env.SECURITY_PASS
@@ -40,7 +59,10 @@ const authenticate = async (req, res) => {
         .json({ error: false, msg: "Usuario habilitado para loguearse" });
 };
 
-const changePassword = () => {};
+const changePassword = () => {
+
+
+};
 
 const forgotPassword = () => {};
 
