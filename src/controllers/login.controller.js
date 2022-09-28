adf72ee053bed564d680b324fe9a716bd3781f16
Chaos <antonioscutari2@hotmail.com>
2022-09-27 22:17:46 -0300

    add authenticate function

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,32 +1,34 @@
-const User = require('../models/User')
+const User = require("../models/User");
 
+const registerPost = () => {};
 
+const confirmUser = () => {};
 
-const registerPost = () => {
+const authenticate = async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
+  !user && res.status(401).send("¡Usuario no existe!");
+  !user.confirmado && res.status(401).send("¡Usuario no confirmado!");
+  const hashPass = CryptoJS.AES.decrypt(
+    user.password,
+    process.env.SECURITY_PASS
+  );
+  const originalPassword = hashPass.toString(CryptoJS.enc.Utf8);
+  const inputPass = req.body.password;
+  originalPassword !== inputPass
+    ? res.status(401).json({ msg: "¡Password inválido!" })
+    : res
+        .status(200)
+        .json({ error: false, msg: "Usuario habilitado para loguearse" });
+};
 
-}
+const changePassword = () => {};
 
-const confirmUser = () => {
-
-}
-
-const authenticate = () => {
-
-}
-
-const changePassword = () => {
-
-}
-
-const forgotPassword = () => {
-
-}
-
+const forgotPassword = () => {};
 
 module.exports = {
-    registerPost,
-    confirmUser,
-    authenticate,
-    changePassword,
-    forgotPassword,
-}
+  registerPost,
+  confirmUser,
+  authenticate,
+  changePassword,
+  forgotPassword,
+};
