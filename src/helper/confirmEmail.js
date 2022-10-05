ddce8913aa7efb7236d1b1be30177d1107319ae7
Tomas Oyarzun <xilent312@gmail.com>
2022-10-05 00:19:07 -0300

    confirm email send

--- src/helper/confirmEmail.js
+++ src/helper/confirmEmail.js
@@ -1,31 +1,44 @@
 const nodemailer = require('nodemailer')
+const sgMail = require('@sendgrid/mail')
 
 const emailRegister = async (datos) => {
-    const transport = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS,
-        }
-      });
+    // const transport = nodemailer.createTransport({
+    //     host: process.env.EMAIL_HOST,
+    //     port: process.env.EMAIL_PORT,
+    //     auth: {
+    //       user: process.env.EMAIL_USER,
+    //       pass: process.env.EMAIL_PASS,
+    //     }
+    //   });
 
-      const {username , email , token } = datos
+      const {username,email , token } = datos
 
-      const info = await transport.sendMail({
-        from : "CellStore - eCommerce celulares",
-        to: email,
-        subject : "Comprueba tu cuenta en CellStore",
-        text : "Comprueba tu cuenta en CellStore",
-        html : `<p>Hola ${username}, comprueba tu cuenta de CellStore.</p>
-        <p> Tu cuenta esta lista, solo debes comprobarla en el siguiente enlace</p>
-         <a href="${process.env.FRONTEND_URL}/confirm/${token}">Comprobar cuenta</a>
-         <p>Si tu no creaste esta cuenta, puedes ignorar este email</p>`
+    //   const info = await transport.sendMail({
+    //     from : "CellStore - eCommerce celulares",
+    //     to: email,
+    //     subject : "Comprueba tu cuenta en CellStore",
+    //     text : "Comprueba tu cuenta en CellStore",
+    //     html : `<p>Hola ${username}, comprueba tu cuenta de CellStore.</p>
+    //     <p> Tu cuenta esta lista, solo debes comprobarla en el siguiente enlace</p>
+    //      <a href="${process.env.FRONTEND_URL}/confirm/${token}">Comprobar cuenta</a>
+    //      <p>Si tu no creaste esta cuenta, puedes ignorar este email</p>`
 
          
-      })
+    //   })
+      
+    sgMail.setApiKey(process.env.SENDGRID_APY_KEY);
+    const msg = {
+        to: email,
+       from: 'tomikapo1160@gmail.com',
+        subject: 'Comprueba tu cuenta en Cell-store',
+        text: 'Comprueba tu cuenta en cell store',
+        html: `<p>Hola ${username}, comprueba tu cuenta de CellStore.</p>
+                <p> Tu cuenta ya esta lista, solo debes comprobarla con el siguiente enlace:
+                <a href="${process.env.FRONTEND_URL}/confirm/${token}">Comprobar Cuenta</a> </p>
+                <p> Si tu no creaste esta cuenta puedes ignorar este mensaje</p>`
+        };
 
-      console.log("Mensaje enviado: %s", info.messageId)
+        await sgMail.send(msg);
 } 
 
 module.exports = emailRegister
